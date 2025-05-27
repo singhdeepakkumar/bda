4b2761d578cf0947dfe5524d6c17042423428e03
singhdeepakkumar <singh.d.kumar@gmail.com>
2025-05-27 14:23:32 +0100

    first commit

---
+++ pipeline/preprocessing.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+def load_data(input_path):
+    return pd.read_csv(input_path)
+
+def preprocess(df):
+    df.dropna(inplace=True)
+    df = df.drop(columns=['customerID'], errors='ignore')
+    df['TotalCharges'] = pd.to_numeric(df['TotalCharges'], errors='coerce').fillna(0)
+    return df
+
+def save_data(df, output_path):
+    df.to_csv(output_path, index=False)
